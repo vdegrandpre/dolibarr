553d7c70a5019130c06a2f2dd48180f1f62abbc6
ATM-Lucas <lucas.mantegari@atm-consulting.fr>
2025-01-27 10:19:54 +0100

    space

--- htdocs/core/class/html.formmail.class.php
+++ htdocs/core/class/html.formmail.class.php
@@ -1708,7 +1708,6 @@ class FormMail extends Form
 
 		  		console.log("We choose a layout for email template=" + template + ", subject="+subject);
 
-
 				console.log("We choose a layout for email template " + template);
 
 				$(".template-option").removeClass("selected");
