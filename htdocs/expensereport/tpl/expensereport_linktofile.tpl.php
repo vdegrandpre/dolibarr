18f616b201eb6ccab8e2cfcd7cb366abd400c466
MDW <mdeweerd@users.noreply.github.com>
2025-01-29 01:05:06 +0100

    Qual: Fix remaining phan notice in expensereport_linktofile

--- htdocs/expensereport/tpl/expensereport_linktofile.tpl.php
+++ htdocs/expensereport/tpl/expensereport_linktofile.tpl.php
@@ -103,7 +103,7 @@ if (!getDolGlobalString('EXPENSEREPORT_DISABLE_ATTACHMENT_ON_LINES')) {
 				print '<div class="photoref backgroundblank">';
 
 				// TODO: Check that $minifile has a proper value here (set in true part of if, not else part).
-				print $thumbshown ? $thumbshown : img_mime($minifile);
+				print $thumbshown ? $thumbshown : ($minifile ? img_mime($minifile) : null);
 
 				print '</div>';
 				if (empty($urlforhref) || empty($thumbshown)) {
